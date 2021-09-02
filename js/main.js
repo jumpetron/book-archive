222dd0a9a2bbca883277bf21965df3d7a76a0ed7
Ahmed Foysal <ahmedfoysal01797@gmail.com>
2021-09-02 17:04:56 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+// Add Spinner
 const toggleSpinner = displayStyle =>{
     document.getElementById('spinner').style.display = displayStyle;
 }
 
+// Add Search Result
+const toggleSearchResult = displayStyle => {
+    document.getElementById('search-result').style.display = displayStyle;
+}
+
+
 
 // Fectch Data
-const searchField = () => {
+const searchBtn = () => {
     const searchBook = document.getElementById('search-box');
     const searchText = searchBook.value;
     
-    toggleSpinner('block')
+    toggleSpinner('block');
 
     // clear data
     searchBook.value = '';
@@ -25,14 +32,32 @@ const searchField = () => {
 
         noResultFound.appendChild(p)
         toggleSpinner('none')
+        
     }
 
     else{
+
+        // books Found
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data.docs))
+
+        // result found
+        fetch(url)
+            .then(res => res.json())
+            .then(data => showResultCount(data.numFound))
     }
     
+}
+
+// Display search result
+const showResultCount = numFound =>{
+    const countFoundResult = document.getElementById('count-result')
+    countFoundResult.textContent = '';
+
+    const p =  document.createElement('p')
+    p.innerHTML = `<p>Search result : ${numFound}</p>`
+    countFoundResult.appendChild(p)
 }
 
 // Display Data
@@ -52,22 +77,24 @@ const displaySearchResult = docs =>{
             p.innerHTML = `<p class="fw-bold text-center text-warning">No Result Found</p>`
 
             noResultFound.appendChild(p)
+
+            toggleSpinner('none')
         }
         
         else{
             docs?.forEach(doc => {
-                console.log(doc)
-
                 const div = document.createElement('div');
                 div.classList.add('col-lg-4');
                 div.innerHTML = `
         <div class="card">
             <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top img-fluid w-100">
             <div class="card-body">
+            <div>
                 <h4 class="card-title">${doc.title}</h4>
                 <p class="card-text text-muted mt-4"><span class="fw-bold">Author Name:</span> ${doc.author_name}</p>
                 <p class="card-text text-muted"><span class="fw-bold">Publisher:</span> ${doc.publisher}</p>
                 <p class="card-text text-muted"><span class="fw-bold">First Published:</span> ${doc.first_publish_year}</p>
+            </div>
             </div>
         </div>
         `
@@ -76,5 +103,6 @@ const displaySearchResult = docs =>{
         }
         
         toggleSpinner('none');
+        toggleSearchResult('block')
 }
 
