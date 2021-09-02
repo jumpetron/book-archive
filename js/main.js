5aa05058e06c1bd911934f4695420060e79a02a4
Ahmed Foysal <ahmedfoysal01797@gmail.com>
2021-09-02 08:24:52 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -3,47 +3,65 @@ const toggleSpinner = displayStyle =>{
 }
 
 
+
 const searchField = () => {
     const searchBook = document.getElementById('search-box');
     const searchText = searchBook.value;
     
 
     toggleSpinner('block')
+
+    // clear data
     searchBook.value = '';
 
     const url = `https://openlibrary.org/search.json?q=${searchText}`
 
+    // load data
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.docs))
 }
 
 const displaySearchResult = docs =>{
-    const searchResult = document.getElementById('search-result');
     
-    searchResult.textContent = '';
-    if(!docs){
-        console.log('mopt')
-    }
-    docs?.forEach (doc => {
-        console.log(doc)
-    
-        const div = document.createElement('div');
-        div.classList.add('col-lg-4');
-        div.innerHTML = `
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+
+        const noResultFound = document.getElementById('no-result-found');
+
+        noResultFound.textContent = '';
+
+        if(docs.length == 0){
+            
+
+            const p = document.createElement('p');
+            p.innerHTML = `<p class="fw-bold text-center text-warning">No Result Found</p>`
+
+            noResultFound.appendChild(p)
+        } 
+        
+        else{
+            docs?.forEach(doc => {
+                console.log(doc)
+
+                const div = document.createElement('div');
+                div.classList.add('col-lg-4');
+                div.innerHTML = `
         <div class="card">
             <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top img-fluid w-100">
             <div class="card-body">
-                <h5 class="card-title">${doc.title}</h5>
-                <p class="card-text">Author Name: ${doc.author_name}</p>
-                <p class="card-text">Publisher: ${doc.publisher}</p>
-                <p class="card-text">First Published: ${doc.first_publish_year}</p>
+                <h4 class="card-title">${doc.title}</h4>
+                <p class="card-text text-muted mt-4"><span class="fw-bold">Author Name:</span> ${doc.author_name}</p>
+                <p class="card-text text-muted"><span class="fw-bold">Publisher:</span> ${doc.publisher}</p>
+                <p class="card-text text-muted"><span class="fw-bold">First Published:</span> ${doc.first_publish_year}</p>
             </div>
         </div>
         `
-
-        searchResult.appendChild(div)
-    })
-    toggleSpinner('none')
+                
+                searchResult.appendChild(div);
+            })
+        }
+        
+        toggleSpinner('none');
 }
 
